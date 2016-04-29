9de2544a1d9fad72d567d52beec0df45d1ca4026
duganjohn <jdugan6@gatech.edu>
2016-04-28 22:20:38 -0400

    Parse headpose. Add 3D distance function.

--- part2.py
+++ part2.py
@@ -6,32 +6,43 @@ def createData(filename):
 	tree = ET.parse(filename)
 	root = tree.getroot()
 
+	def parseData(data):
+		parsed_data = data.split()
+		for d in range(len(parsed_data)):
+			headpose_data.append([float(parsed_data[0]), float(parsed_data[1]), float(parsed_data[2])])
+
 	def parseAndAddArrayData(data):
 		# Data is currently [x1, x2, x3, ... y1, y2, y3, ...]
 		parsed_data = data.split()
 		for j in range(len(parsed_data) / 2):
 			n = j + len(parsed_data) / 2
-			all_data[i].append((parsed_data[j], parsed_data[n]))
+			feature_data[i].append((float(parsed_data[j]), float(parsed_data[n])))
 
 	# Set up data
-	all_data = []
+	feature_data = []
+	headpose_data = []
 	i = 0
 	for frame in root.findall('frame'):
-		all_data.append([])
+		feature_data.append([])
 
 		for landmark in frame.findall('landmarks'):
 			for feature in landmark.findall('data'):
 				parseAndAddArrayData(feature.text)
 
-		# print
+		for pose in frame.findall('headpose'):
+			for coord in pose.findall('data'):
+				parseData(coord.text)
+
 		i += 1
 
-	return all_data
+	return feature_data, headpose_data
 
 def distance(p1, p2):
-	return ((( float(p2[0]) - float(p1[0]) )**2) + (( float(p2[1]) - float(p1[1]) )**2))**0.5
+	if len(p1) == 2:
+		return ((( float(p2[0]) - float(p1[0]) )**2) + (( float(p2[1]) - float(p1[1]) )**2))**0.5
+	return ((( float(p2[0]) - float(p1[0]) )**2) + (( float(p2[1]) - float(p1[1]) )**2) + ( float(p2[2]) - float(p1[2]) )**2)**0.5
 
-def plotThatData():
+def plotData():
 	import numpy as np
 	import matplotlib.pyplot as plt
 
@@ -55,30 +66,50 @@ def plotThatData():
 
 	plt.show()
 
+def calculate_asymmetry():
+	# These are the pairs from left to right features across the face
+	# All of these following numbers are 1 less than their documentation, because they are indices in the data array
+	pairs = [[4, 5], [3, 6], [2, 7], [1, 8], [0, 9],											# Eyebrows
+			 [19, 28], [20, 27], [21, 26], [22, 25], [23, 30], [24, 29],						# Eyes
+			 [14, 18], [24, 29],																# Nose
+			 [31, 37], [32, 36], [33, 35], [42, 38], [41, 39], [41, 39], [43, 45], [46, 48]]	# Lips
+	middle = [10, 11, 12, 13, 16, 34, 40, 44, 47]
+
+	print headpose_data[0]
+	for pair in pairs:
+		frame = 0
+		mid_feature = 12
+
+		p1 = training_data[frame][pair[0]]
+		# print p1
+		p1 = mapPoint(headpose_data[frame], p1)
+
+		p2 = training_data[frame][pair[1]]
+		p2 = mapPoint(headpose_data[frame], p2)
+
+		mid_point = training_data[frame][mid_feature]
+		mid_point = mapPoint(headpose_data[frame], mid_point)
+
+		dis1 = distance(p1, mid_point)
+		dis2 = distance(p2, mid_point)
+		print dis1, dis2, abs(dis1 - dis2)
+
+# Map 2D points into 3D space with 3D vector
+def mapPoint(vector, point):
+	mappedPoint = []
+	for v in vector:
+		tot = 0
+		for c in point:
+			tot += v * c
+		mappedPoint.append(tot)
+	return mappedPoint
+
 
 # training_data = createData('../eye_move_lr.MP4_intraface_data.xml')
-training_data = createData('../face_move_left_right[2].MP4_intraface_data.xml')
+training_data, headpose_data = createData('../face_move_left_right[2].MP4_intraface_data.xml')
 
-# These are the pairs from left to right features across the face
-# All of these following numbers are 1 less than their documentation, because they are indices in the data array
-pairs = [[4, 5], [3, 6], [2, 7], [1, 8], [0, 9],											# Eyebrows
-		 [19, 28], [20, 27], [21, 26], [22, 25], [23, 30], [24, 29],						# Eyes
-		 [14, 18], [24, 29],																# Nose
-		 [31, 37], [32, 36], [33, 35], [42, 38], [41, 39], [41, 39], [43, 45], [46, 48]]	# Lips
-middle = [10, 11, 12, 13, 16, 34, 40, 44, 47]
-
-for pair in pairs:
-	frame = 329
-	mid_feature = 12
-	dis1 = distance(training_data[frame][pair[0]], training_data[frame][mid_feature])
-	dis2 = distance(training_data[frame][pair[1]], training_data[frame][mid_feature])
-	print dis1, dis2, abs(dis1 - dis2)
-
-
-
-
-
-
+frame = 0
+calculate_asymmetry()
 
 # test_data = createData('../social.MP4_intraface_data.xml')
 # # test_data = createData('../face_move_left_right.MP4_intraface_data.xml')
